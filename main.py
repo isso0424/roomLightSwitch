4c8b8812eb194b299d81f094c240eaf7379fdbd6
isso0424 <isso04240129@gmail.com>
2020-07-15 06:27:15 +0900

    [Add]created arg parser

---
+++ main.py
@@ -0,0 +1,68 @@
+import bluetooth
+from bluetooth.ble import DiscoveryService, GATTRequester
+import time
+import sys
+
+
+class Driver(object):
+    handle = 0x16
+    commands = {
+        'press': '\x57\x01\x00',
+        'on': '\x57\x01\x01',
+        'off': '\x57\x01\x02',
+    }
+
+    def __init__(self, device, timeout_secs=None):
+        self.device = device
+        self.timeout_secs = timeout_secs if timeout_secs else 5
+        self.req = None
+
+    def _connect(self):
+        self.req = GATTRequester(self.device, False)
+        self.req.connect(True, 'random')
+        connect_start_time = time.time()
+        while not self.req.is_connected():
+            if time.time() - connect_start_time >= self.timeout_secs:
+                raise RuntimeError('Connection to {} timed out after {} seconds'
+                                   .format(self.device, self.timeout_secs))
+
+    def __enter__(self):
+        self._connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.req.disconnect()
+
+    def run_command(self, command):
+        return self.req.write_by_handle(self.handle, self.commands[command])
+
+
+def operate(timeout, addr, command):
+    with Driver(device=addr, timeout_secs=timeout) as driver:
+        print('Connected!')
+        command_list = {
+            'press': '\x57\x01\x00',
+            'on': '\x57\x01\x01',
+            'off': '\x57\x01\x02',
+        }
+        if command in command_list.keys():
+            driver.run_command(command)
+        else:
+            ValueError("Command is press, on or off.")
+        print("completed")
+
+def get_command():
+    args = sys.argv
+    if len(args) == 1:
+        raise ValueError("invalid args")
+
+    command = args[1]
+
+    if command not in ["on", "off"]:
+        raise ValueError("invalid args")
+
+    return command
+
+if __name__ == "__main__":
+    command = get_command()
+    operate(10, "D8:79:65:69:B7:AE", command)
