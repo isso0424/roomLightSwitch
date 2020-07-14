55608e446625637247b95dd5430a6155daba9e01
isso0424 <isso04240129@gmail.com>
2020-07-15 04:49:12 +0900

    [Add]on/offのコマンドの受け取りができるようになった

--- main.go
+++ main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
 func main() {
+  commandPtr, err := getCommand()
+  if err != nil {
+    log.Fatalln("Error: ", err)
+  }
+
+  switch (*commandPtr) {
+  case "on":
+    fmt.Println("on")
+  case "off":
+    fmt.Println("off")
+  }
+}
+
+func getCommand() (*string, error) {
+  flag.Parse()
+  flags := flag.Args()
+
+  if len(flags) == 0 {
+    return nil, &getCommandError{}
+  }
+
+  command := flags[0]
+
+  if command != "on" && command != "off" {
+    return nil, &getCommandError{}
+  }
+
+  return &command, nil
+}
+
+type getCommandError struct {
+}
+
+func(ptr *getCommandError) Error() string {
+  return "Invalid command"
 }
